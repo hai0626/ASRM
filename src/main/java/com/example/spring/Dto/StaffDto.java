4b4a456fcb8fc7ab98c3044af8d3e394a1484000
hai0626 <hai.nguyen26062000@yahoo.com.vn>
2024-02-12 10:46:04 +0700

    First mapper

--- src/main/java/com/example/spring/Dto/StaffDto.java
+++ src/main/java/com/example/spring/Dto/StaffDto.java
@@ -1,15 +1,2 @@
-package com.example.spring.Dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class StaffDto {
-    private String staffId;
-    private String staffPassword;
-    private int attempts;
-
+package com.example.spring.Dto;public class StaffDto {
 }
---
+++ src/main/java/com/example/spring/Dto/StaffLoginDto.java
@@ -0,0 +1,14 @@
+package com.example.spring.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StaffDto {
+    private String staffId;
+    private String staffPassword;
+
+}
